565911a48e8864b932c465392b41153a73414fdb
PRISCILLA BÁRBOSA <priscillabarbosa@outlook.com>
2024-04-15 21:12:04 -0300

    MÓDULO 18 - exercicio_grunt

---
+++ MÓDULO 18 - exercicio/src/scripts/main.js
@@ -0,0 +1,11 @@
+document.getElementById("maisInfo").addEventListener("submit", function(event){
+    event.preventDefault()
+    var campoA = document.getElementById("campoA").value;
+    var campoB = document.getElementById("campoB").value;
+    
+    if(parseInt(campoB) > parseInt(campoA)) {
+        document.getElementById("mensagem").innerHTML = "Formulário válido!";
+    } else {
+        document.getElementById("mensagem").innerHTML = "Formulário inválido!";
+    }
+});
