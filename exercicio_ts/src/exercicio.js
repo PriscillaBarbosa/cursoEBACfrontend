5e267f73e01c56dd8f09c388004a72ac7100bafa
PRISCILLA B <priscillabarbosa@outlook.com>
2024-07-30 20:28:03 -0300

    MÓDULO 26 - exercicio TypeScript
    Utilizando o TypeScript e a tipagem escreva duas funções: uma função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles, e uma função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome;

---
+++ exercicio_ts/src/exercicio.js
@@ -0,0 +1,12 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudar(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso
+console.log(multiplicar(10, 3)); // Saída: 30
+console.log(saudar("Priscilla")); // Saída: Olá Priscilla
---
+++ exercicio_ts/src/exercicio.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplos de uso
+console.log(multiplicar(10, 3)); // Saída: 30
+console.log(saudar("Priscilla")); // Saída: Olá Priscilla
